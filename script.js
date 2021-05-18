c1108fc96b39fe7059a7d6ed828d68764f654cb6
May Comia <pmaycomia@gmail.com>
2021-05-18 22:10:28 +0200

    Create javaScript variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const score = document.querySelectorAll('.score')
+const timer = document.querySelectorAll('.time-left')
