43d1b18040e81a2e71a17e9fdc9e663a273295c5
May Comia <pmaycomia@gmail.com>
2021-05-20 23:46:00 +0200

    Add a random square function in js, change colos in css

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 const square = document.querySelectorAll('.square')
 const mole = document.querySelectorAll('.mole')
-const score = document.querySelectorAll('.score')
-const timer = document.querySelectorAll('.time-left')
+const score = document.querySelectorAll('#score')
+const timer = document.querySelectorAll('#time-left')
+
+let result = 0
+
+//create randomSquare function
+function randomSquare() {
+    square.forEach(className => {
+        className.classList.remove('mole')
+    })
+
+    let randomPosition = square[Math.floor(Math.random() * 9)]
+    randomPosition.classList.add('mole')
+}
+
+//add mole in class list
+
+//hitposition is random
+
+//foreach square add eventlistener 'mouseup'
+//if id is same as hit position add 1 point
+
+//create movemole function
+//create a timer var
+//setinterval
+
+//create countdown function
